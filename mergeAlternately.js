d226ab3cbf2dca53f43cb676d0c4a9ce9e559f58
jsimonassi <jsimonassi@id.uff.br>
2024-05-21 22:41:31 -0300

    Question

---
+++ mergeAlternately.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {string}
+ */
+var mergeAlternately = function(word1, word2) {
+    let result=''
+    let maxIteration =Math.max(word1.length,word2.length);
+    for(let i=0;i<maxIteration;i++){
+        if(i<word1.length) result+=word1[i]
+        if(i<word2.length) result+=word2[i]
+    }
+    return result
+};
+
+mergeAlternately("abc", "pqr");
